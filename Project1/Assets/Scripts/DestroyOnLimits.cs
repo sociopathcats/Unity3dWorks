c8c26435461539b1a98d39ea65390e36c47ae144
Sociopathcats <97188870+sociopathcats@users.noreply.github.com>
2022-07-01 16:02:43 +0300

    updated

--- Project1/Assets/Scripts/DestroyOnLimits.cs
+++ Project1/Assets/Scripts/DestroyOnLimits.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class DestroyOnLimits : MonoBehaviour
 {
-    public GameManager gameManager;
-    private float leftLimit=-220f;
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +15,15 @@ public class DestroyOnLimits : MonoBehaviour
    
     void Update()
     {
-        if (transform.position.x < leftLimit)
-        {
-            DestroyObject(gameObject);
-            gameManager.isOrbitCenterOn = false;
-        }
-        
+        OnDestroy(-110);
     }
+    
+    private void OnDestroy(float leftLimit)
+    {   if(transform.position.x < leftLimit)
+        {
+           
+            Destroy(gameObject);  
+          }
+     }
+        
 }
